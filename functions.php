d60d87cecb88b2d1a5de4a3b006cb2a1186bff05
Ronald Huereca <ronald@mediaron.com>
2019-06-09 18:35:04 -0500

    Splitting the footer to make it modular.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-// Enqueue the parent theme's CSS
+// Enqueue the parent theme's CSS.
 add_action( 'wp_enqueue_scripts', 'twentynineteen_child_enqueue_scripts' );
 function twentynineteen_child_enqueue_scripts() {
 	wp_enqueue_style(
@@ -12,7 +12,14 @@ function twentynineteen_child_enqueue_scripts() {
 	);
 }
 
+// Include the theme's header.
 add_action( 'twentynineteen_child_header', 'twentynineteen_child_header' );
 function twentynineteen_child_header() {
 	include 'includes/header.php';
+}
+
+// Include the theme's footer.
+add_action( 'twentynineteen_child_footer', 'twentynineteen_child_footer' );
+function twentynineteen_child_footer() {
+	include 'includes/footer.php';
 }
