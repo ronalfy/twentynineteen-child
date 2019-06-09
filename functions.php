6c0103ffc91c52485bf293af740ed7b1edd98803
Ronald Huereca <ronald@mediaron.com>
2019-06-09 18:06:33 -0500

    Enqueuing the main theme's stylesheet.

--- functions.php
+++ functions.php
@@ -3,5 +3,11 @@
 // Enqueue the parent theme's CSS
 add_action( 'wp_enqueue_scripts', 'twentynineteen_child_enqueue_scripts' );
 function twentynineteen_child_enqueue_scripts() {
-	wp_enqueue_style( 'twentynineteen' );
+	wp_enqueue_style(
+		'twentynineteen',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		'1.4',
+		'all'
+	);
 }
