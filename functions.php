e3db52f301911ac5837f49a752fc4a4c36832f51
Ronald Huereca <ronald@mediaron.com>
2019-06-09 18:02:25 -0500

    Creating twentynineteen child theme.

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// Enqueue the parent theme's CSS
+add_action( 'wp_enqueue_scripts', 'twentynineteen_child_enqueue_scripts' );
+function twentynineteen_child_enqueue_scripts() {
+	wp_enqueue_style( 'twentynineteen' );
+}
